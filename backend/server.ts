401a302cd7a2af40baa051a37d4b9bf044dc609a
RokuX9 <shibta007@gmail.com>
2023-03-07 01:13:58 +0200

    Some Changes

--- backend/server.ts
+++ backend/server.ts
@@ -1,57 +1,113 @@
 import * as mediasoup from "mediasoup";
 import express from "express";
-import { db } from "./firebase/firebase"
-import {} from "firebase-admin/firestore"
+import { db } from "./firebase/firebase";
 import { authenticate, AuthRequest } from "./middlewares/auth";
 
 declare global {
-    namespace Express {
-        interface Request extends AuthRequest{}
-    }
+  namespace Express {
+    interface Request extends AuthRequest {}
+  }
 }
 
 interface Room {
-    router : mediasoup.types.Router,
-    transporter: mediasoup.types.Transport,
+  router: mediasoup.types.Router;
+  transporter: mediasoup.types.Transport;
 }
 
-let roomsArray: Array<Room>= []
+let roomsArray: Array<Room> = [];
 const topLevelAsync = async () => {
+  process.on("exit", () => {
+    console.log("exiting");
+    mediaWorker.close();
+  });
 
-const app = express()
-const mediaWorker = await mediasoup.createWorker()
-const webRtcServer = await mediaWorker.createWebRtcServer({listenInfos: [{
-    protocol: "udp",
-    ip: "10.100.102.37"
-}]})
+  const app = express();
+  const mediaWorker = await mediasoup.createWorker();
+  const webRtcServer = await mediaWorker.createWebRtcServer({
+    listenInfos: [
+      {
+        protocol: "udp",
+        ip: "10.100.102.37",
+      },
+    ],
+  });
+  app.get("/make-room", async (req, res, next) => {
+    app.use(express.json());
+    //app.use(authenticate)
 
-app.use(express.json())
-//app.use(authenticate)
-
-app.get("/make-room", async (req, res, next) => {
-    const router = await mediaWorker.createRouter()
-    const transporter = await router.createWebRtcTransport({webRtcServer})
-    const roomRef = db.collection("rooms").doc()
+    const router = await mediaWorker.createRouter({
+      mediaCodecs: [
+        {
+          kind: "audio",
+          mimeType: "audio/opus",
+          clockRate: 48000,
+          channels: 2,
+        },
+        {
+          kind: "video",
+          mimeType: "video/VP8",
+          clockRate: 90000,
+          parameters: {
+            "x-google-start-bitrate": 1000,
+          },
+        },
+      ],
+    });
+    const transporter = await router.createWebRtcTransport({ webRtcServer });
+    const roomRef = db.collection("rooms").doc();
     roomRef.set({
-        id: transporter.id,
-        iceParameters: transporter.iceParameters,
-        iceCandidates: transporter.iceCandidates,
-        dtlsParameters: transporter.dtlsParameters,
-        rtpCapabilities: router.rtpCapabilities
-    })
-    transporter.on("@newproducer", () => {
-        console.log("got a new producer")
-    })
-    res.status(200).send(roomRef.id)
-})
+      id: transporter.id,
+      iceParameters: transporter.iceParameters,
+      iceCandidates: transporter.iceCandidates,
+      dtlsParameters: transporter.dtlsParameters,
+      rtpCapabilities: router.rtpCapabilities,
+    });
+    const usersCollection = roomRef.collection("users");
+    usersCollection.onSnapshot((snapshot) => {
+      snapshot.docChanges().forEach((change1) => {
+        if (change1.type === "added") {
+          const sendTransporterCollection =
+            change1.doc.ref.collection("sendTransporter");
+          const data = change1.doc.data();
+          const { sendParameters, rtpCapabilities } = data;
+          transporter.connect({ dtlsParameters: sendParameters });
+          router.canConsume(rtpCapabilities);
 
-app.listen(3000, () => {
-    console.log("server running")
-})
+          sendTransporterCollection.onSnapshot((snapshot) => {
+            snapshot.docChanges().forEach(async (change2) => {
+              if (change2.type === "added") {
+                const data = change2.doc.data();
+                const userParameters: mediasoup.types.ProducerOptions = {
+                  kind: data.kind,
+                  rtpParameters: data.rtpParameters,
+                };
+                const producer = await transporter.produce(userParameters);
+                await change2.doc.ref.set({ serverAnswer: producer.id });
+                const consumer = await transporter.consume({
+                  producerId: producer.id,
+                  rtpCapabilities,
+                  paused: true,
+                });
+                const recvTransportCollection =
+                  change1.doc.ref.collection("recvTransporter");
+                await recvTransportCollection.add({
+                  consumerId: consumer.id,
+                  producerId: producer.id,
+                  kind: producer.kind,
+                  rtpParameters: consumer.rtpParameters,
+                });
+              }
+            });
+          });
+        }
+      });
+    });
+    res.status(200).send(roomRef.id);
+  });
 
-process.on("exit", () => {
-    mediaWorker.close()
-})
-}
+  app.listen(3000, () => {
+    console.log("server running");
+  });
+};
 
-topLevelAsync()
+topLevelAsync();
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,107 +1,218 @@
 import React from "react";
 import { servers } from "./utils/servers.js";
-import { getAuth, GoogleAuthProvider, signInWithPopup} from "firebase/auth"
-import {useAuthState} from "react-firebase-hooks/auth";
-import {auth, db} from "./firebase/firebase.js";
 import {
-	collection,
-	addDoc,
-	setDoc,
-	onSnapshot,
-	getDoc,
-	updateDoc,
-	doc,
-	DocumentData,
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signInWithRedirect,
+} from "firebase/auth";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth, db } from "./firebase/firebase.js";
+import {
+  collection,
+  addDoc,
+  setDoc,
+  onSnapshot,
+  getDoc,
+  updateDoc,
+  doc,
+  DocumentData,
+  DocumentReference,
+  CollectionReference,
 } from "firebase/firestore";
-import {Device, types} from "mediasoup-client"
+import { Device, types } from "mediasoup-client";
 import "./App.css";
+import { Consumer } from "mediasoup-client/lib/Consumer.js";
 
 function App() {
-	const provider = new GoogleAuthProvider()
-	const [user] = useAuthState(auth)
-	const device = new Device()
-	
-	const [callId, setCallId] = React.useState<String>("");
+  const provider = new GoogleAuthProvider();
+  const [user] = useAuthState(auth);
+  const device = new Device();
 
-	const localVideoRef = React.useRef<HTMLVideoElement | null>(null);
-	const remoteVideoRef = React.useRef<HTMLVideoElement | null>(null);
-	const callIdInputRef = React.useRef<HTMLInputElement | null>(null);
+  const [callId, setCallId] = React.useState<String>("");
+  const [remoteStream, setRemoteStream] = React.useState(new MediaStream());
 
-	
+  const localVideoRef = React.useRef<HTMLVideoElement | null>(null);
+  const remoteVideoRef = React.useRef<HTMLVideoElement | null>(null);
+  const callIdInputRef = React.useRef<HTMLInputElement | null>(null);
 
-	return (
-		<div className="App">
-			<header className="header">
-				<h1 className="title">High Chat</h1>
-				{user && <button onClick={async () => {
-					console.log(user)
-					await auth.signOut()
-				}}>Disconnect</button>}
-			</header>
-			{user ? <main className="main">
-				<div className="views">
-					<video
-						ref={localVideoRef}
-						className="userVideo"
-						id="localVideo"
-						autoPlay
-						playsInline
-					></video>
-					<video
-						ref={remoteVideoRef}
-						className="userVideo"
-						id="remoteVideo"
-						autoPlay
-						playsInline
-					></video>
-				</div>
-				<button onClick={async () => {
-					const roomRef = (await getDoc(doc(db, `rooms/${callIdInputRef.current!.value}`))).data()
-					await device.load({routerRtpCapabilities: roomRef!.rtpCapabilities})
-					const serverOptions : types.TransportOptions = {
-						id: roomRef!.id,
-						iceParameters: roomRef!.iceParameters,
-						iceCandidates: roomRef!.iceCandidates,
-						dtlsParameters: roomRef!.dtlsParameters
+  return (
+    <div className="App">
+      <header className="header">
+        <h1 className="title">High Chat</h1>
+        {user && (
+          <button
+            onClick={async () => {
+              console.log(user);
+              await auth.signOut();
+            }}
+          >
+            Disconnect
+          </button>
+        )}
+      </header>
+      {user ? (
+        <main className="main">
+          <div className="views">
+            <video
+              ref={localVideoRef}
+              className="userVideo"
+              id="localVideo"
+              autoPlay
+              playsInline
+            ></video>
+            <video
+              ref={remoteVideoRef}
+              className="userVideo"
+              id="remoteVideo"
+              autoPlay
+              playsInline
+            ></video>
+          </div>
+          <button
+            onClick={async () => {
+              const roomRef = doc(db, `rooms/${callIdInputRef.current!.value}`);
+              const roomData = (await getDoc(roomRef)).data();
+              await device.load({
+                routerRtpCapabilities: roomData!.rtpCapabilities,
+              });
+              const serverOptions: types.TransportOptions = {
+                id: roomData!.id,
+                iceParameters: roomData!.iceParameters,
+                iceCandidates: roomData!.iceCandidates,
+                dtlsParameters: roomData!.dtlsParameters,
+              };
+              const sendTransporter = device.createSendTransport(serverOptions);
+              const recvTransporter = device.createRecvTransport(serverOptions);
 
+              let userRef: DocumentReference;
+              let sendTransportCollection: CollectionReference;
+              let recvTransportCollection: CollectionReference;
+              const streamArray: Array<MediaStreamTrack> = [];
+              sendTransporter.on(
+                "connect",
+                async ({ dtlsParameters }, callback, errback) => {
+                  const usersCollection = collection(roomRef, "users");
+                  try {
+                    userRef = await addDoc(usersCollection, {
+                      userId: auth.currentUser!.uid,
+                      transportId: sendTransporter.id,
+                      name: auth.currentUser!.displayName,
+                      sendParameters: dtlsParameters,
+                      rtpCapabilities: device.rtpCapabilities,
+                    });
+                    sendTransportCollection = collection(
+                      userRef,
+                      "sendTransporter"
+                    );
+                    recvTransportCollection = collection(
+                      userRef,
+                      "recvTransporter"
+                    );
+                    callback();
+                  } catch (err: any) {
+                    errback(err);
+                  }
+                  onSnapshot(recvTransportCollection, async (snapshot) => {
+                    snapshot.docChanges().forEach(async (change) => {
+                      if (change.type === "added") {
+                        const data = change.doc.data();
+                        const consumer = await recvTransporter.consume({
+                          id: data.consumerId,
+                          producerId: data.producerId,
+                          kind: data.kind,
+                          rtpParameters: data.rtpParameters,
+                        });
+                        consumer.resume();
+                        remoteStream.addTrack(consumer.track);
+                      }
+                    });
+                  });
+                }
+              );
 
-					}
-					const sendTransporter = device.createSendTransport(serverOptions)
-					const recvTransporter = device.createRecvTransport(serverOptions)
-					const stream = await navigator.mediaDevices.getUserMedia({video:true, audio:true})
-					const audioTrack = stream.getAudioTracks()[0]
-					const videoTrack = stream.getVideoTracks()[0]
-					console.log(stream, audioTrack, videoTrack)
-					const videoProducer = await sendTransporter.produce({track: videoTrack})
-					const audioProducer = await sendTransporter.produce({track: audioTrack})
-					
-				}}>
-					Start call Session
-				</button>
-				<p>
-					Your call ID is: <span>{callId}</span>
-				</p>
-				<div>
-					<p>Enter Call ID</p>
-					<input
-						type="text"
-						ref={callIdInputRef}
-					/>
-					<button>
-						Answer Call
-					</button>
-				</div>
-			</main> : <main>
-						<button onClick={async () => {
-							const result = await signInWithPopup(auth, provider)
-						}}>Press to sign In</button>
-				</main>}
-			
-			<footer className="footer">
-				<p className="copyright">Made by Dean Nash</p>
-			</footer>
-		</div>
-	);
+              sendTransporter.on(
+                "produce",
+                async (parameters, callback, errback) => {
+                  try {
+                    const sendTransporterRef = await addDoc(
+                      sendTransportCollection,
+                      {
+                        kind: parameters.kind,
+                        rtpParameters: parameters.rtpParameters,
+                        transportId: sendTransporter.id,
+                      }
+                    );
+
+                    let producerId: string;
+                    onSnapshot(sendTransporterRef, async (snapshot) => {
+                      producerId = snapshot.data()!.serverAnswer;
+                      callback({ id: producerId });
+                    });
+                  } catch (err: any) {
+                    errback(err);
+                  }
+                }
+              );
+
+              recvTransporter.on(
+                "connect",
+                async ({ dtlsParameters }, callback, errback) => {
+                  console.log("hello");
+                  try {
+                    await setDoc(userRef, {
+                      recvParameters: dtlsParameters,
+                    });
+                    callback();
+                  } catch (err: any) {
+                    errback(err);
+                  }
+                }
+              );
+
+              const stream = await navigator.mediaDevices.getUserMedia({
+                video: true,
+                audio: true,
+              });
+              localVideoRef.current!.srcObject = stream;
+              const audioTrack = stream.getAudioTracks()[0];
+              const videoTrack = stream.getVideoTracks()[0];
+              const videoProducer = await sendTransporter.produce({
+                track: videoTrack,
+              });
+              const audioProducer = await sendTransporter.produce({
+                track: audioTrack,
+              });
+            }}
+          >
+            Start call Session
+          </button>
+          <p>
+            Your call ID is: <span>{callId}</span>
+          </p>
+          <div>
+            <p>Enter Call ID</p>
+            <input type="text" ref={callIdInputRef} />
+            <button>Answer Call</button>
+          </div>
+        </main>
+      ) : (
+        <main>
+          <button
+            onClick={async () => {
+              const result = await signInWithRedirect(auth, provider);
+            }}
+          >
+            Press to sign In
+          </button>
+        </main>
+      )}
+
+      <footer className="footer">
+        <p className="copyright">Made by Dean Nash</p>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
